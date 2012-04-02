959cd0e81147d7bf8123659d8c6e136612cfd92a
Rod Paddock <rodneypaddock@gmail.com>
2012-04-02 09:02:30 -0500

    create non posgres version of installs

---
+++ no_postgres/updateubuntu.sh
@@ -0,0 +1,13 @@
+
+sudo apt-get update 
+
+#install some python utils for apt help
+echo "installing python apt utils"
+sudo apt-get -y install python-software-properties 
+
+#update all of the package
+echo "upgrading packages this takes a HECK OF A LONG TIME go get some coffee. Get me a cup too"
+sudo apt-get upgrade -y  
+
+
+echo "Package update complete!"
