a508da69111cd747ddcce5a7160d79ca1327ca5a
Rod Paddock <rodneypaddock@gmail.com>
2012-11-06 15:23:48 -0600

    add install apache script

---
+++ apacheinstall.sh
@@ -0,0 +1,10 @@
+sudo apt-get install apache2-mpm-prefork  -y
+
+ 
+ sudo apt-get install apache2-prefork-dev -y
+
+ 
+ sudo apt-get install libapr1-dev -y
+
+ 
+sudo apt-get install libaprutil1-dev -y
